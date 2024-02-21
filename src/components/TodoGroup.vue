b2ef8bdf1853f9d94f99281a4bd7507ac6005671
Gabriel Santos <gabrieljorgesilvasantos@outlook.com>
2024-02-20 22:07:30 -0300

    refactor

--- src/components/TodoGroup.vue
+++ src/components/TodoGroup.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h3>{{ groupLabel }}</h3>
+    <h3>{{ groupLabel[props.status] }}</h3>
     <ul>
-      <li v-for="todo in pendingTodos" :key="todo.id">{{ todo.title }}</li>
+      <li v-for="todo in todoList" :key="todo.id">{{ todo.title }}</li>
     </ul>
   </div>
 </template>
@@ -10,6 +10,7 @@
 <script setup lang="ts">
 import { TodoStatus, type Todo } from '@/types'
 import { computed } from 'vue';
+import useTodos from '@/store/useTodos';
 
 interface Props {
     status: TodoStatus
@@ -17,25 +18,12 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const pendingTodos: Todo[] = [
-  {
-    id: 1,
-    title: 'Learn Vuejs',
-    description: 'Watch videos',
-    status: TodoStatus.Pending
-  }
-]
+const { getTodoByStatus } = useTodos()
+const todoList = getTodoByStatus(props.status)
 
-const groupLabel = computed(() => {
-    switch(props.status) {
-        case TodoStatus.Pending:
-            return "Pending";
-        case TodoStatus.InProgress:
-            return "In Progress";
-        case TodoStatus.Completed:
-            return "Completed";
-        default:
-            return "Todo Group"
-    }
-})
+const groupLabel = {
+  [TodoStatus.Pending]: "Pending",
+  [TodoStatus.InProgress]: "In progress",
+  [TodoStatus.Completed]: "Completed"
+}
 </script>
---
+++ src/store/useTodos.ts
@@ -0,0 +1,31 @@
+import { TodoStatus, type Todo } from "@/types"
+import { computed, reactive } from "vue"
+
+interface TodoStore {
+    [TodoStatus.Pending]: Todo[]
+    [TodoStatus.InProgress]: Todo[]
+    [TodoStatus.Completed]: Todo[]
+}
+
+const defaultVal = {
+    [TodoStatus.Pending]: [
+        {
+            id: 1,
+            title: 'Learn Vuejs',
+            description: 'Watch videos',
+            status: TodoStatus.Pending
+        },
+    ],
+    [TodoStatus.InProgress]: [],
+    [TodoStatus.Completed]: []
+}
+
+const todoStore = reactive<TodoStore>(defaultVal)
+
+export default () => {
+    const getTodoByStatus = (TodoStatus: TodoStatus) => {
+        return computed(() => todoStore[TodoStatus])
+    }
+
+    return { getTodoByStatus }
+}
