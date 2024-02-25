d18723ea8c01976a40281929dd131658dda3f1af
Gabriel Santos <gabrieljorgesilvasantos@outlook.com>
2024-02-25 17:02:10 -0300

    feat: layout form

--- src/components/CreatedTodo.vue
+++ src/components/CreatedTodo.vue
@@ -4,16 +4,18 @@
       Add New
     </h3>
     <template v-else>
-      <form @submit.prevent="handleOnSubmit">
+      <form class="container-form" @submit.prevent="handleOnSubmit">
         <div>
-          <input type="text" placeholder="Title" v-model="newTodo.title" />
+          <input class="input-title" type="text" placeholder="Title" v-model="newTodo.title" />
         </div>
-        <div>
-          <textarea type="text" placeholder="Description" v-model="newTodo.description" />
+        <div class="todo-description">
+          <textarea class="textarea" type="text" placeholder="Description" v-model="newTodo.description" />
         </div>
-
-        <button type="submit">Submit</button>
-        <button type="button" @click="resetForm">Cancel</button>
+        
+        <div class="buttons-form">
+        <button type="submit" class="submit">Submit</button>
+        <button type="button" class="cancel" @click="resetForm">Cancel</button>
+        </div>
       </form>
     </template>
   </div>
@@ -55,5 +57,44 @@ const handleOnSubmit = () => {
 <style scoped>
 .buttonNew {
   cursor: pointer;
+}
+
+.container-form {
+  justify-content: center;
+  align-items: center;
+}
+
+.todo-description{
+  margin-top: 7px;
+}
+
+.submit{
+  font-weight: bold;
+  color: black;
+  background-color: rgba(0, 128, 0, 0.582);
+  border-radius: 15px 15px;
+  border-color: gray;
+}
+
+.cancel{
+  font-weight: bold;
+  color: black;
+  background-color: rgba(255, 0, 0, 0.651);
+  border-radius: 15px 15px;
+  border-color: gray;
+}
+
+.buttons-form{
+  flex-wrap: wrap;
+  display: flex;
+  gap: 1em;
+}
+
+.textarea{
+  border-radius: 8px 8px;
+}
+
+.input-title{
+  border-radius: 8px 8px;
 }
 </style>
